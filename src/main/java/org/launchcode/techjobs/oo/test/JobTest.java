74b6bc61c59cffacfeffecf705d439af117dd561
Telly Teague <telly.teague@gmail.com>
2021-10-23 21:37:14 -0500

    Make changes not specified in instructions

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -5,6 +5,7 @@ import org.junit.runners.JUnit4;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import org.launchcode.techjobs.oo.CoreCompetency;
 import org.launchcode.techjobs.oo.Employer;
@@ -52,30 +53,49 @@ public class JobTest {
         assertTrue( job.getCoreCompetency().equals(cc) );
     }
 
+    @Test
     public void testJobsForEquality()
     {
         Job job = new Job();
         assertFalse( job.equals(new Job()) );
     }
 
+    @Test
     public void testSettingJobId()
     {
         Job job = new Job();
         Job job2 = new Job();
+        assertNotEquals( job.getId(), job2.getId() );
     }
 
+    @Test
     public int testSettingId( int id )
     {
         Job job = new Job();
         return job.getId();
     }
 
+    @Test
     public void testToStringHandlesEmptyField()
     {
+        String jobName = "name";
+        String notAvail = "Data Not Available";
+
         Job job = new Job();
-        assertEquals( "", job.toString() );
+        job.setName( jobName );
+
+        StringBuilder sb = new StringBuilder( "\n" );
+        sb.append( "ID: " + job.getId() + "\n" );
+        sb.append( "Name: " + jobName + "\n" );
+        sb.append( "Employer: " + notAvail + "\n" );
+        sb.append( "Location: " + notAvail + "\n" );
+        sb.append( "Position: " + notAvail + "\n" );
+        sb.append( "Core: " + notAvail + "\n" );
+
+        assertEquals( sb.toString(), job.toString() );
     }
 
+    @Test
     public void testToStringStartsAndEndsWithNewLine()
     {
         Job job = new Job();
@@ -84,11 +104,26 @@ public class JobTest {
         assertEquals( '\n', job.toString().charAt( job.toString().length() - 1) );
     }
 
+    @Test
     public void testToStringContainsCorrectLabelsAndData()
     {
-      Job job = new Job();
-      job.setName( "name" );
+      String jobName = "name";
+      String employer = "DST";
+      String location = "Kansas City, MO";
+      String position = "Senior Software Engineer";
+      String core = "Java";
 
-      assertEquals( "\nID: 1\nName: name\n", job.toString() );
+      Job job = new Job( jobName, employer, location, position, core );
+
+      StringBuilder sb = new StringBuilder( "\n" );
+      sb.append( "ID: " + job.getId() + "\n" );
+      sb.append( "Name: " + jobName + "\n" );
+      sb.append( "Employer: " + employer + "\n" );
+      sb.append( "Location: " + location + "\n" );
+      sb.append( "Position: " + position + "\n" );
+      sb.append( "Core: " + core + "\n" );
+
+
+      assertEquals( sb.toString(), job.toString() );
     }
 }
